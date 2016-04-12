47795ce1073007ec6230c486b54bd2261b927b04
German Escobar <german.escobarc@gmail.com>
2016-04-12 16:52:34 -0500

    Implement create expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,12 +1,6 @@
 class ExpensesController < ApplicationController
   def index
-    @month = Date.new(year, month, 1)
-    @expenses = Expense.between(@month..@month.end_of_month).order("date DESC")
-    @expenses = @expenses.where(type: params[:type]) if params[:type]
-    @expenses = @expenses.where(category_id: params[:category]) if params[:category]
-
-    @total = @expenses.map(&:amount).reduce(&:+)
-
+    populate_index_data
     @tab = :expenses
   end
 
@@ -14,12 +8,30 @@ class ExpensesController < ApplicationController
     @expense = Expense.new
   end
 
+  def create
+    @expense = Expense.create(expenses_params)
+    populate_index_data if @expense.valid?
+  end
+
   private
+    def populate_index_data
+      @month = Date.new(year, month, 1)
+      @expenses = Expense.between(@month..@month.end_of_month).order("date DESC")
+      @expenses = @expenses.where(type: params[:type]) if params[:type]
+      @expenses = @expenses.where(category_id: params[:category]) if params[:category]
+
+      @total = @expenses.map(&:amount).reduce(&:+)
+    end
+
     def year
       params[:year] ? params[:year].to_i : Date.current.year
     end
 
     def month
       params[:month] ? params[:month].to_i : Date.current.month
+    end
+
+    def expenses_params
+      params.require(:expense).permit(:type, :date, :concept, :category_id, :amount)
     end
 end
